3c498feca50522df5e64d3e70b26926d0de06b3a
AnitaGuillen <ana.guillen@iteso.mx>
2024-04-12 18:19:25 -0600

    Temporizador 3s funcionando

--- source/main.c
+++ source/main.c
@@ -1,49 +1,72 @@
-/*
- * Copyright 2016-2024 NXP
- * All rights reserved.
+/** Assignment 9: Nokia.
  *
- * SPDX-License-Identifier: BSD-3-Clause
+ * Code that displays a different image each 3 seconds.
+ *
+ * There are 6 available images
+ *
+ * Ana Isabel Guillen Gonzalez 	739422
+ * Patricio Coz Puig			739393
+ *
+ * 04/05/2024, Electronic Engineering ITESO.
  */
 
-/**
- * @file    Tarea_9.c
- * @brief   Application entry point.
- */
-#include <stdio.h>
-#include "board.h"
-#include "peripherals.h"
-#include "pin_mux.h"
-#include "clock_config.h"
-#include "MK64F12.h"
-#include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
+#include <stdint.h>
+#include <stdbool.h>
+#include "NVIC.h"
+#include "PIT_WITH_SDK.h"
+#include "GPIO_WITH_SDK.h"
 
-/* TODO: insert other definitions and declarations here. */
 
-/*
- * @brief   Application entry point.
- */
-int main(void) {
+uint8_t g_three_scnds = 0;
 
-    /* Init board hardware. */
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitBootPeripherals();
-#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
-    /* Init FSL debug console. */
-    BOARD_InitDebugConsole();
-#endif
+void flag_three_scnds()
+{
+	g_three_scnds = 1;
+}
 
-    PRINTF("Hello World\r\n");
+void NVIC_init(void)
+{
+	/**Sets the threshold for interrupts, if the interrupt has higher priority constant
+	 *  that the BASEPRI, the interrupt will not be attended **/
+	NVIC_set_basepri_threshold(PRIORITY_6);
 
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
-    }
-    return 0 ;
+	/**Enables and sets the priority for the PIT timers interrupts**/
+	NVIC_enable_interrupt_and_priotity(PIT_CH0_IRQ, PRIORITY_1);
+}
+
+int main(void)
+{
+	CLOCK_SetSimSafeDivs();
+	/**Initialize the PIT timers needed**/
+	pit_init_module();
+	PIT_SetTimerPeriod(PIT, kPIT_Chnl_0, 31500000);
+
+	pit_timer_init_callback(kPIT_Chnl_0, flag_three_scnds);
+	PIT_EnableInterrupts(PIT, kPIT_Chnl_0, kPIT_TimerInterruptEnable);
+
+
+	NVIC_init();
+	NVIC_global_enable_interrupts;
+
+	gpio_pin_config_t output_config = {
+			        kGPIO_DigitalOutput,
+			        0,
+	};
+
+
+	CLOCK_EnableClock(kCLOCK_PortD);
+	PORT_SetPinMux(PORTD, 0, kPORT_MuxAsGpio);
+	GPIO_PinInit(GPIOD, 0, &output_config);
+	g_three_scnds = 0;
+	PIT_StartTimer (PIT, kPIT_Chnl_0);
+	for(;;)
+	{
+
+		if (1 == g_three_scnds)
+		{
+			GPIO_PortToggle(GPIOD, 1);
+			g_three_scnds = 0;
+		}
+	}
+	return 0;
 }
