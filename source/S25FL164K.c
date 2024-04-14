95ce14a4363e575405e931f87b804ef932a05766
AnitaGuillen <ana.guillen@iteso.mx>
2024-04-13 18:56:07 -0600

    Configuracion 3

--- source/S25FL164K.c
+++ source/S25FL164K.c
@@ -61,8 +61,8 @@ void s25fl_init()
 	masterConfig.ctarConfig.cpha                          = kDSPI_ClockPhaseSecondEdge;
 	masterConfig.ctarConfig.direction                     = kDSPI_MsbFirst;
 	masterConfig.ctarConfig.pcsToSckDelayInNanoSec        = 0U;
-	masterConfig.ctarConfig.lastSckToPcsDelayInNanoSec    = 1000000000U;
-	masterConfig.ctarConfig.betweenTransferDelayInNanoSec = 0U;
+	masterConfig.ctarConfig.lastSckToPcsDelayInNanoSec    = 0U;
+	masterConfig.ctarConfig.betweenTransferDelayInNanoSec = 1000000000U;
 
 	masterConfig.whichPcs           = kDSPI_Pcs1;
 	masterConfig.pcsActiveHighOrLow = kDSPI_PcsActiveLow;
